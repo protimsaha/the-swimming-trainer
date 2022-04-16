63f80518875c7b8b617f1aac99b2f9c3b6fb5341
protimsaha <protimsaha750@gmail.com>
2022-04-17 01:08:24 +0600

    everything added

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Copyright Clamied Footer @ 2022</p>
+        </div>
+    );
+};
+
+export default Footer;
